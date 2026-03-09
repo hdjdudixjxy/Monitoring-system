4228da8f71a34ff04bfdc87b0dde1d457833f7d9
hdjdudixjxy <crisaaron_1@hotmail.com>
2026-03-09 17:01:58 -0500

    confidence=0.4 en videoVision.py

--- videoVision.py
+++ videoVision.py
@@ -130,7 +130,7 @@ def main():
             results = model.predict(
                 source=frame_to_process,
                 imgsz=640,
-                conf=0.25,
+                conf=0.4,
                 verbose=False
             )
 
